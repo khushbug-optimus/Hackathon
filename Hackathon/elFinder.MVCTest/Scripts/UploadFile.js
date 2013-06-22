821c8475f3b8b56621589bea9042eec4ce0eeeb9
khushbu garg <khushbu.garg@optmusinfo.com>
2013-06-22 13:44:13 +0530

    Includer upload file

---
+++ Hackathon/elFinder.MVCTest/Scripts/UploadFile.js
@@ -0,0 +1,42 @@
+﻿function FtpConnection() {
+    $.ajax({
+        url: "/Upload/TestFtpConnection",
+        cache: false,
+        type: "POST",
+        dataType: "json",
+        async: false,
+        data: { ip: $("#txtIpAddress").val(),
+            port: $("#txtPortNumber").val(),
+            username: $("#txtUserName").val(),
+            password: $("#txtPassword").val()
+        },
+        success: function (data) {
+            if (data) {
+                alert("Connected successfully");
+            }
+            else {
+                alert("Unable to connect Please try again later");
+            }
+        }
+    });
+}
+
+function FileUpload() {
+    $.ajax({
+        url: "/Upload/UploadFiles",
+        cache: false,
+        type: "POST",
+        dataType: "json",
+        async: false,
+        data: { 
+        },
+        success: function (data) {
+            if (data) {
+                alert("Connected successfully");
+            }
+            else {
+                alert("Unable to connect Please try again later");
+            }
+        }
+    });
+}
